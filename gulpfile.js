69c78e9e6bc76c9bf678280e0ad6ab4313cfe9fb
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-02-10 16:58:47 -1000

    do most of large layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
 
   return gulp.src('./sass/*.scss')
-  .pipe(sass( { errLogToConsole: true } ))
-  .pipe(gulp.dest('./public/css')); });
+    .pipe(sass( {errlogToConsole: true } ))
+    .pipe(gulp.dest('./public/css'));
+
+});
 
 gulp.task('watch_sass', function() {
 
